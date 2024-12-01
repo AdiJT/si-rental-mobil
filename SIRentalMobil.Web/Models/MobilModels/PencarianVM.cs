72d330298eee4b06529bfbb851a40454f1e677d7
AdiJT <aditaklal@gmail.com>
2024-12-01 10:27:53 +0800

    Selesai pencarian dan detail mobil

--- SIRentalMobil.Web/Models/MobilModels/PencarianVM.cs
+++ SIRentalMobil.Web/Models/MobilModels/PencarianVM.cs
@@ -1,4 +1,5 @@
 ﻿using SIRentalMobil.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace SIRentalMobil.Web.Models.MobilModels;
 
@@ -7,7 +8,12 @@ public class PencarianVM
     public required List<Mobil> DaftarMobil { get; set; }
     public required bool LuarKota { get; set; }
     public required bool? Sopir { get; set; }
+
+    [Required(ErrorMessage = "Harus diiisi")]
     public required DateOnly? TanggalMulai { get; set; }
+
+    [Required(ErrorMessage = "Harus diiisi")]
     public required DateOnly? TanggalAkhir { get; set; }
+
     public required int JumlahHariSewa { get; set; }
 }
